759e04214fd3ba0d4187bbba8ff381db5385bf21
stuarttiedemann <stuarttiedemann@yahoo.com>
2015-11-18 22:08:18 -0500

    updated controllers

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,31 @@
-var stocksApp = angular.module('stocksApp',[]);
+var stocksApp = angular.module('stocksApp',['ngRoute']);
+	
+	stocksApp.config(function($routeProvider,$locationProvider){
+		$routeProvider.when('/',{
+			templateUrl:'stock-summary.html',
+			controller: 'stocksController'
+		}).
+		when('/stock-detail',{
+			templateUrl:'stock-detail.html',
+			controller: 'stocksDetailController'
+		}).
+		otherwise({
+			redirectTo: 'stock-summary.html'
+		});
+	});
 
 
+stocksApp.controller('stocksDetailController',function ($scope, $location){
 
-stocksApp.controller('stocksController',function ($scope, $http){
+ $scope.detailsFunction = function(){
+     	$scope.detailsList = allData;
+     	// console.log($scope.detailsList);
+     }
+})	
 
+stocksApp.controller('stocksController',function ($scope, $http, $location){
+	allData =[];
+	$scope.detailsList = [];
 	$scope.getStocks = function(){
 		var encodedTickers= encodeURIComponent($scope.userStocks);
 		var stocksInput= {};
@@ -14,8 +36,7 @@ stocksApp.controller('stocksController',function ($scope, $http){
 			var commaCount=0;
 			for(i=0; i< $scope.userStocks.length; i++){
 				if($scope.userStocks[i]===','){
-					commaCount ++;
-					
+					commaCount ++;	
 				}
 			}
 				if(commaCount > 0){
@@ -25,39 +46,20 @@ stocksApp.controller('stocksController',function ($scope, $http){
 					$scope.listOfStocks = [stockData.query.results.quote];
 					commaCount = 0;
 				}
-			
-			// $scope.listOfStocks = [stockData.query.results.quote];
-			// console.log($scope.listOfStocks);
-			// $scope.loadStock($scope)
 		});
-		// $scope.listOfStocks = [
-  //           {
-  //               Symbol: "Goog",
-  //               Change: "+1.2",
-  //               Name: "Google"
-  //           },
-  //           {
-  //               Symbol: "Yhoo",
-  //               Change: "-1.2",
-  //               Name: "Yahoo"
-  //           },
-  //           {
-  //               Symbol: "X",
-  //               Change: "+2.2",
-  //               Name: "Trinity Trains"
-  //           }
-  //           ];
 	}
 
 	$scope.loadStock = function(stockData){
             // init array
             $scope.dataList = [];
+            
             for(name in stockData){
                 $scope.dataList.push({
                     prop:name,
                     value: stockData[name]
                 });
             }
+            // console.log($scope.dataList);
             $scope.mainData = [];
             // symbol
             $scope.mainData.push($scope.dataList[0].value);
@@ -73,11 +75,35 @@ stocksApp.controller('stocksController',function ($scope, $http){
             $scope.mainData.push($scope.dataList[21].value);
             //day high
             $scope.mainData.push($scope.dataList[22].value);
-            console.log($scope.mainData);
+            // console.log($scope.mainData);
+           
+
+            allData.push({val: $scope.dataList[38].value});
+            allData.push({val: $scope.dataList[5].value});
+            allData.push({val: $scope.dataList[4].value});
+            allData.push({val: $scope.dataList[11].value});
+            allData.push({val: $scope.dataList[21].value});
+            allData.push({val: $scope.dataList[22].value});
+            allData.push({val: $scope.dataList[46].value});
+            allData.push({val: $scope.dataList[23].value});
+            allData.push({val: $scope.dataList[24].value});
+            allData.push({val: $scope.dataList[74].value});
+            allData.push({val: $scope.dataList[2].value});
+            allData.push({val: $scope.dataList[48].value});
+            allData.push({val: $scope.dataList[50].value});
+            allData.push({val: $scope.dataList[60].value});
+            allData.push({val: $scope.dataList[62].value});
+            allData.push({val: $scope.dataList[13].value});
+            allData.push({val: $scope.dataList[81].value});
+            allData.push({val: $scope.dataList[70].value});
+            
+
+            console.log(allData);
         }
-        
+    
+
 	$scope.getChangeClass = function (change){
-		// console.log(change);
+		
             if(change.indexOf('+') > -1){
                 return 'change-positive';
             }else if(change.indexOf('-') > -1){
@@ -86,5 +112,10 @@ stocksApp.controller('stocksController',function ($scope, $http){
             	return 'unchanged';
             }
         }
-
+    $scope.detailsPage = function(){
+    	$location.path('/stock-detail')
+    }
+    $scope.stockSummary = function(){
+    	$location.path('/');
+    }
 })
